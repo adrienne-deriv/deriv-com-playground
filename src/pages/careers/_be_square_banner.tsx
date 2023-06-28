17dd8cafc79ac73b1a228d18f029c12fd36e2cf6
adrienne-rio <adrienne@deriv.com>
2023-06-28 11:42:10 +0800

    feat: spells

--- src/pages/careers/_be_square_banner.tsx
+++ src/pages/careers/_be_square_banner.tsx
@@ -127,7 +127,7 @@ const BeSquareBanner = () => {
                 <LeftWrapper>
                     <LeftChild>
                         <StyledHeader as="h3" type="section-title">
-                            Join the Deriv graduate programme
+                            Join the Deriv graduate programmes
                         </StyledHeader>
                         <StyledText>
                             Are you a go-getter who loves learning and taking on new challenges?
