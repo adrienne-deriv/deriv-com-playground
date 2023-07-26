d49d11a43c74e6867c38ac30826c2dfdd21e2856
adrienne-rio <adrienne@deriv.com>
2023-07-26 11:09:11 +0800

    feat: fail me for cached

--- src/features/pages/home/hero/content/data.ts
+++ src/features/pages/home/hero/content/data.ts
@@ -18,5 +18,5 @@ export type THomeHeroImage = {
 export const header_items: THeaderItem[] = [
     { id: 0, text: '_t_One broker,_t_' },
     { id: 1, text: '_t_countless kuru kuru_t_' },
-    { id: 2, text: '_t_opportunities_t_' },
+    { id: 2, text: '_t_opportun
 ]
