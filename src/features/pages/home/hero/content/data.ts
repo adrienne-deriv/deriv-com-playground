db4c8964a8b517ee8b7751e7310b888f538a8453
adrienne-rio <adrienne@deriv.com>
2023-07-26 11:44:04 +0800

    feat: fixed mistake

--- src/features/pages/home/hero/content/data.ts
+++ src/features/pages/home/hero/content/data.ts
@@ -18,5 +18,5 @@ export type THomeHeroImage = {
 export const header_items: THeaderItem[] = [
     { id: 0, text: '_t_One broker,_t_' },
     { id: 1, text: '_t_countless trading_t_' },
-    { id: 2, text: '_t_opportunities and kururin her!_t_' },
+    { id: 2, text: '_t_opportunities and kururin here!_t_' },
 ]
